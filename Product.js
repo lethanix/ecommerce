003181734e44bd623092c3b98b5306725c6bb793
lethanix <louismurguia@proton.me>
2024-01-10 08:59:15 -0600

    Add simple Product class

---
+++ Product.js
@@ -0,0 +1,17 @@
+export class Product {
+  #title;
+  #description;
+  #price;
+  #thumbnail;
+  #code;
+  #stock;
+
+  constructor({ title, description, price, thumbnail, code, stock }) {
+    this.#title = title;
+    this.#description = description;
+    this.#price = price;
+    this.#thumbnail = thumbnail;
+    this.#code = code;
+    this.#stock = stock;
+  }
+}
--- index.js
+++ index.js
@@ -1,83 +1,84 @@
 class ProductManager {
-    static #id = 0;
-    #products;
+  static #id = 0;
+  #products;
 
-    constructor() {
-        this.#products = []
+  constructor() {
+    this.#products = [];
+  }
+
+  // Private function isRequired will log an error
+  // if there is no input value for the parameter.
+  addProduct(
+    {
+      title = this.#isRequired("Title"),
+      description = this.#isRequired("Description"),
+      price = this.#isRequired("Price"),
+      thumbnail = this.#isRequired("Thumbnail"),
+      code = this.#isRequired("Code"),
+      stock = this.#isRequired("Stock"),
+    },
+  ) {
+    if (!this.#isCodeUnique(code)) {
+      console.error(`Code ${code} is not unique. Please provide another code.`);
+      return;
     }
 
-    // Private function isRequired will log an error
-    // if there is no input value for the parameter.
-    addProduct({ title = this.#isRequired("Title"),
-        description = this.#isRequired("Description"),
-        price = this.#isRequired("Price"),
-        thumbnail = this.#isRequired("Thumbnail"),
-        code = this.#isRequired("Code"),
-        stock = this.#isRequired("Stock") }) {
+    ProductManager.#id++;
+    const newProduct = {
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+      id: ProductManager.#id,
+    };
 
-        if (!this.#isCodeUnique(code)) {
-            console.error(`Code ${code} is not unique. Please provide another code.`)
-            return;
-        }
+    this.#products.push(newProduct);
+  }
 
-        ProductManager.#id++;
-        const newProduct = {
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock,
-            id: ProductManager.#id,
-        };
+  getProducts() {
+    return this.#products;
+  }
 
-        this.#products.push(newProduct);
-
-
+  getProductById(id) {
+    for (const product of this.#products) {
+      if (id === product.id) return product;
     }
 
-    getProducts() {
-        return this.#products;
+    console.log("Not found");
+  }
+
+  #isRequired(argument = "") {
+    console.error(`${argument} is required`);
+  }
+
+  #isCodeUnique(code) {
+    if (this.#products.length === 0) {
+      return true;
     }
 
-    getProductById(id) {
-        for (const product of this.#products) {
-            if (id === product.id) return product;
-        }
-
-        console.log("Not found");
+    for (const product of this.#products) {
+      return code === product.code ? false : true;
     }
-
-    #isRequired(argument = "") {
-        console.error(`${argument} is required`);
-    }
-
-    #isCodeUnique(code) {
-        if (this.#products.length === 0) {
-            return true;
-        }
-
-        for (const product of this.#products) {
-            return code === product.code ? false : true;
-        }
-    }
+  }
 }
 
 const manager = new ProductManager();
 console.log(manager.getProducts());
 
 const testProduct = {
-    title: "producto prueba",
-    description: "Este es un producto prueba",
-    price: 200,
-    thumbnail: "Sin imagen",
-    code: "abc123",
-    stock: 25,
-}
+  title: "producto prueba",
+  description: "Este es un producto prueba",
+  price: 200,
+  thumbnail: "Sin imagen",
+  code: "abc123",
+  stock: 25,
+};
 
 manager.addProduct(testProduct);
 console.log(manager.getProducts());
 
 manager.addProduct(testProduct);
 
-console.log(manager.getProductById(3))
+console.log(manager.getProductById(3));
