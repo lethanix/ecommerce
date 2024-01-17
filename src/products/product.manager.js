2977d8e88accf87f02d6094f2ac4a4e4f39d3cb2
louiscmm <louiscmm@gmail.com>
2024-01-17 00:27:17 +0000

    Update functions to use async

--- src/products/product.manager.js
+++ src/products/product.manager.js
@@ -2,18 +2,15 @@ import { Product } from "./product.js";
 import { ProductMemoryRepository } from "./product.memory.repository.js";
 
 export class ProductManager {
-  static #id = 0;
-  #products;
   #repository;
 
   constructor(repositoryType) {
     if (repositoryType === "MEMORY") {
       this.#repository = new ProductMemoryRepository();
     }
-    this.#products = [];
   }
 
-  addProduct(newProduct) {
+  async addProduct(newProduct) {
     // Only instances of Product class can be added
     const isValid = newProduct instanceof Product;
     if (!isValid) {
@@ -21,38 +18,26 @@ export class ProductManager {
     }
 
     // Unique code is needed for each product 
-    if (!this.#isCodeUnique(newProduct.code)) {
-      console.error(`Code ${newProduct.code} is not unique. Please provide another code.`);
-      return;
+    const isUnique = await this.#isCodeUnique(newProduct.code);
+    if (!isUnique) {
+      throw new Error(`Unable to add new product to ProductManager: Code ${newProduct.code} is not unique.`);
     }
 
-    this.#products.push(newProduct);
+    await this.#repository.addProduct(newProduct);
   }
 
-  getProducts() {
-    return this.#products.map(product => product.toJSON())
-    // return [...this.#products];
+  async getProducts() {
+    return await this.#repository.getProducts();
   }
 
-  getProductById(id) {
-    for (const product of this.#products) {
-      if (id === product.id) return product.toJSON();
-    }
-
-    console.log("Not found");
+  async getProductById(id) {
+    return await this.#repository.getProductById(id);
   }
 
-  #isRequired(argument = "") {
-    console.error(`${argument} is required`);
-  }
+  async #isCodeUnique(code) {
+    const products = await this.#repository.getProducts();
+    const codeFound = products.some(p => p.code === code);
 
-  #isCodeUnique(code) {
-    if (this.#products.length === 0) {
-      return true;
-    }
-
-    for (const product of this.#products) {
-      return code === product.code ? false : true;
-    }
+    return !codeFound;
   }
 }
