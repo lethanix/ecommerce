47533af6bff175b8f9faf65fc8741c2218c636e0
lethanix <louismurguia@proton.me>
2024-01-22 10:49:58 -0600

    Add express config

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import { ProductManager } from "./products/product.manager.js";
-import { Product } from "./products/product.js";
 import express from "express";
 import "dotenv/config.js"; // Load environment variables from .env
 
 const REPOSITORY = process.env.REPOSITORY || "FS";
 const MOCK_DATA = process.env.MOCK_DATA || "MOCK_DATA.json";
 
-const app = express();
 const PORT = 8080;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const manager = new ProductManager(REPOSITORY, MOCK_DATA);
 
@@ -15,7 +17,7 @@ app.get("/products", async (req, res) => {
   try {
     const products = await manager.getProducts();
 
-    let { limit } = req.query;
+    const { limit } = req.query;
 
     if (limit) {
       const limitedProducts = products.filter((_, idx) => idx < limit);
@@ -24,16 +26,14 @@ app.get("/products", async (req, res) => {
     }
 
     res.send(products);
-
   } catch (error) {
     res.send({ error: `Unable to load products` });
     console.log(error);
   }
-
 });
 
 app.get("/products/:pid", async (req, res) => {
-  let pid = Number(req.params.pid);
+  const pid = Number(req.params.pid);
   let product;
   try {
     product = await manager.getProductById(pid);
@@ -42,10 +42,8 @@ app.get("/products/:pid", async (req, res) => {
     res.send({ error: `Unable to find product with id ${pid}` });
     console.log(error);
   }
-
 });
 
 app.listen(PORT, () => {
   console.log(`Server listing port ${PORT}`);
-})
-
+});
