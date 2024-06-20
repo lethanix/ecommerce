c6cfd07cef19250c0734c2d6d5fe4f49131e93a0
lethanix <louismurguia@proton.me>
2024-06-19 18:17:23 -0600

    Add delete event to remove product from db/fs and the HTML

--- src/public/js/realtime.js
+++ src/public/js/realtime.js
@@ -1,6 +1,7 @@
 // Create socket connection
 const socket = io();
 
+//*** Form Event
 // Get the form information and send it to the server
 const form = document.getElementById("product-form");
 
@@ -16,7 +17,7 @@ form.addEventListener("submit", (e) => {
 	}).then((_) => form.reset());
 });
 
-// Events
+//*** Socket Events
 socket.on("server:product:added", (product) => {
 	const productList = document.getElementById("product-list");
 
@@ -27,18 +28,35 @@ socket.on("server:product:added", (product) => {
 	productList.appendChild(fragment);
 });
 
+socket.on("server:product:deleted", (pid) => {
+	const product = document.getElementById(pid);
+	product.remove();
+});
+
+//*** Delete Button Event
+function deleteProduct(event) {
+	const pid = event.getAttribute("data-id");
+	fetch(`/api/realtimeproducts/${pid}`, { method: "DELETE" }).then((res) => {
+		if (res.status !== 200) {
+			alert(`Error while deleting product: ${pid}`);
+		}
+	});
+}
+
 /**
- *
+ * Function to update the UI
  * @param {Product} product - Creates the HTML element of a product
  * @returns {HTMLDivElement} div - Returns the div element created
  */
 const productUI = (product) => {
 	const div = document.createElement("div");
+	div.setAttribute("id", product.id);
 	const p = document.createElement("p");
 	p.innerHTML = product.title;
 
 	const button = document.createElement("button");
 	button.setAttribute("data-id", product.id);
+	button.setAttribute("onclick", "deleteProduct(this);");
 	button.innerHTML = "delete";
 
 	div.append(p, button);
