b32ef5ed9589595467c8d5942999d5ce1bea77c1
lethanix <louismurguia@proton.me>
2024-07-05 13:33:04 +0000

    Add Cart manager, model and route

---
+++ src/routes/cart.route.js
@@ -0,0 +1,55 @@
+import express from "express";
+import {Cart} from "../models/cart.js";
+import { CartManager } from "../managers/cart.manager.js";
+
+
+const CART_DATA = process.env.CART_DATA || "cart.json";
+
+export const router = express.Router();
+const manager = new CartManager(CART_DATA);
+
+
+/**
+ * Create new cart
+ */
+router.post("/", async (_, res) => {
+    try {
+        const cart = new Cart();
+        await manager.addCart(cart);
+
+        res.status(200).send({ status: "Successful", message: "Cart added" });
+        // res.json(cart);
+    } catch (addCartError) {
+        return res.status(400).send({ status: "Error", error: `${addCartError}` });
+    }
+});
+
+/**
+ * Obtain the products in the cart with the given ID
+ */
+router.get("/:cid(\\d+)", async (req, res) => {
+    const cid = Number(req.params.cid);
+
+    try {
+        const products = await manager.getProducts(cid);
+        res.json(products);
+    } catch (cartIdError) {
+        return res.status(400).send({ status: "Error", error: `${cartIdError}` });
+    }
+});
+
+router.post("/:cid(\\d+)/product/:pid", async (req, res) => {
+    const cid = Number(req.params.cid);
+    const pid = req.params.pid;
+
+    try {
+        await manager.addProduct(cid, pid);
+        res
+            .status(200)
+            .send({ status: "Successful", message: "Product added to cart" });
+    } catch (addProductError) {
+        return res
+            .status(400)
+            .send({ status: "Error", error: `${addProductError}` });
+    }
+});
