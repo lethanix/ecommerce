b971fcf28ae816beae16d14a8e285ac93db959a0
lethanix <louismurguia@proton.me>
2024-07-09 22:38:50 -0600

    Add PUT and DELETE endpoints for carts

--- src/managers/mongo/cart.manager.js
+++ src/managers/mongo/cart.manager.js
@@ -72,4 +72,109 @@ export default class CartManager {
 		const products = cart.products;
 		return [...products];
 	}
+
+	/**
+	 * Delete a product from the cart with the given ID
+	 * @param {String} cartId ID of the cart to update
+	 * @param {String} productId ID of the product to be deleted from the cart
+	 */
+	async deleteProduct(cartId, productId) {
+		const identifier = { key: "id", value: cartId };
+		const cart = await this.#repository.getDataByIdentifier(identifier);
+
+		if (cart === null) {
+			throw new Error(
+				`Unable to add product to cart: cart id ${cartId} not found`,
+			);
+		}
+
+		// Verify if product is in the cart
+		const idx = cart.products.findIndex(
+			(p) => p.product._id.toString() === productId,
+		);
+
+		// If product is in the cart, remove it
+		if (idx === -1) {
+			throw new Error(
+				`Unable to delete product from the cart: product id ${productId} not found`,
+			);
+		}
+		cart.products = cart.products.filter(
+			(p) => p.product._id.toString() !== productId,
+		);
+
+		return await this.#repository.updateDataByIdentifier(identifier, cart);
+	}
+
+	/**
+	 * Delete all products from the cart with the given ID
+	 * @param {String} cartId ID of the cart to update
+	 */
+	async deleteAllProducts(cartId) {
+		const identifier = { key: "id", value: cartId };
+		const cart = await this.#repository.getDataByIdentifier(identifier);
+
+		if (cart === null) {
+			throw new Error(
+				`Unable to add product to cart: cart id ${cartId} not found`,
+			);
+		}
+
+		cart.products = [];
+
+		return await this.#repository.updateDataByIdentifier(identifier, cart);
+	}
+
+	/**
+	 * Change all products from the cart with the given ID
+	 * @param {String} cartId ID of the cart to update
+	 * @param {Object} products Array of products with the new data
+	 */
+	async updateCart(cartId, products) {
+		const identifier = { key: "id", value: cartId };
+		const cart = await this.#repository.getDataByIdentifier(identifier);
+
+		if (cart === null) {
+			throw new Error(
+				`Unable to add product to cart: cart id ${cartId} not found`,
+			);
+		}
+
+		cart.products = products;
+
+		return await this.#repository.updateDataByIdentifier(identifier, cart);
+	}
+
+	/**
+	 * Change the product quantity from the cart with the given ID
+	 * @param {String} cartId ID of the cart to update
+	 * @param {String} productId ID of the product to update
+	 * @param {Number} quantity New quantity
+	 */
+	async updateProduct(cartId, productId, quantity) {
+		const identifier = { key: "id", value: cartId };
+		const cart = await this.#repository.getDataByIdentifier(identifier);
+
+		if (cart === null) {
+			throw new Error(
+				`Unable to add product to cart: cart id ${cartId} not found`,
+			);
+		}
+
+		// Verify if product is in the cart
+		const idx = cart.products.findIndex(
+			(p) => p.product._id.toString() === productId,
+		);
+
+		// If product is in the cart, update it
+		if (idx === -1) {
+			throw new Error(
+				`Unable to update product from the cart: product id ${productId} not found`,
+			);
+		}
+
+		cart.products[idx].quantity = quantity;
+
+		return await this.#repository.updateDataByIdentifier(identifier, cart);
+	}
 }
--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -32,7 +32,7 @@ router.get("/:cid", async (req, res) => {
 	}
 });
 
-router.post("/:cid/product/:pid", async (req, res) => {
+router.post("/:cid/products/:pid", async (req, res) => {
 	const { cid, pid } = req.params;
 
 	try {
@@ -44,5 +44,75 @@ router.post("/:cid/product/:pid", async (req, res) => {
 		});
 	} catch (addCartError) {
 		return res.status(400).send({ status: "Error", error: `${addCartError}` });
+	}
+});
+
+router.delete("/:cid/products/:pid", async (req, res) => {
+	const { cid, pid } = req.params;
+
+	try {
+		const result = await cartService.deleteProduct(cid, pid);
+		res.status(200).send({
+			status: "Successful",
+			message: `Product ${pid} deleted from the cart`,
+			cid: result,
+		});
+	} catch (deleteProductError) {
+		return res
+			.status(400)
+			.send({ status: "Error", error: `${deleteProductError}` });
+	}
+});
+
+router.delete("/:cid", async (req, res) => {
+	const cid = req.params.cid;
+
+	try {
+		const result = await cartService.deleteAllProducts(cid);
+		res.status(200).send({
+			status: "Successful",
+			message: "All products deleted from the cart",
+			cid: result,
+		});
+	} catch (deleteAllProductsError) {
+		return res
+			.status(400)
+			.send({ status: "Error", error: `${deleteAllProductsError}` });
+	}
+});
+
+router.put("/:cid", async (req, res) => {
+	const cid = req.params.cid;
+	const products = req.body.products;
+
+	try {
+		const result = await cartService.updateCart(cid, products);
+		res.status(200).send({
+			status: "Successful",
+			message: "Cart products updated",
+			cid: result,
+		});
+	} catch (updateCartError) {
+		return res
+			.status(400)
+			.send({ status: "Error", error: `${updateCartError}` });
+	}
+});
+
+router.put("/:cid/products/:pid", async (req, res) => {
+	const { cid, pid } = req.params;
+	const quantity = req.body.quantity;
+
+	try {
+		const result = await cartService.updateProduct(cid, pid, quantity);
+		res.status(200).send({
+			status: "Successful",
+			message: `Product quantity updated to ${quantity}`,
+			cid: result,
+		});
+	} catch (updateProductError) {
+		return res
+			.status(400)
+			.send({ status: "Error", error: `${updateProductError}` });
 	}
 });
