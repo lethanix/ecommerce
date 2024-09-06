b3b55d80914d61da63a1e9eb443d765bd9e4b0f9
lethanix <louismurguia@proton.me>
2024-09-06 01:27:18 +0000

    Add Database Factory class

---
+++ src/db/db.factory.js
@@ -0,0 +1,35 @@
+import {
+	CART_REPOSITORY_NAME,
+	DB_TYPE,
+	PRODUCT_REPOSITORY_NAME,
+	USER_REPOSITORY_NAME,
+} from "../utils.js";
+
+export class DatabaseFactory {
+	async selectDatabase(db = DB_TYPE) {
+		switch (db) {
+			case "fs":
+				// TODO: Implement UserDAO for file system persistance
+				// const FileSystemUserDao = await import("./filesystem/user.dao.js");
+				return new Error("FileSystem DAO for Users is not implemented");
+				const FileSystemProductDao = await import("./filesystem/product.dao.js");
+				const FileSystemCartDao = await import("./filesystem/cart.dao.js");
+				// return {
+				// 	UserDao: new FileSystemUserDao(USER_REPOSITORY_NAME),
+				// 	ProductDao: new FileSystemProductDao(PRODUCT_REPOSITORY_NAME),
+				// 	CartDao: new FileSystemCartDao(CART_REPOSITORY_NAME)
+				// }
+		
+			case "mongo":
+			default:
+				const MongoUserDao = await import("./mongo/user.dao.js");
+				const MongoProductDao = await import("./mongo/product.dao.js");
+				const MongoCartDao = await import("./mongo/cart.dao.js");
+				return {
+					UserDao: new MongoUserDao(USER_REPOSITORY_NAME),
+					ProductDao: new MongoProductDao(PRODUCT_REPOSITORY_NAME),
+					CartDao: new MongoCartDao(CART_REPOSITORY_NAME)
+				}
+		}
+	}
+}
